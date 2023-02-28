cbf07016a19b6658533a6daee0a0c3d703611c86
Mina Sabry <73604557+minasabry1@users.noreply.github.com>
2023-02-28 16:14:58 +0200

    PORTS_DRIVER

---
+++ unit 7/lesson 4/Section_Port_Driver/Lesson4_Unit7_Drivers/Debug/makedep.mk
@@ -0,0 +1,12 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+HAL\KEY_PAD\KEY_PAD.c
+
+HAL\LCD_DRIVER\LCD.c
+
+main.c
+
+MCAL\PORT_DDR.c
+
---
+++ unit 7/lesson 4/Section_Port_Driver/Lesson4_Unit7_Drivers/main.c
@@ -0,0 +1,35 @@
+/*
+ * Lesson4_Unit7_Drivers.c
+ *
+ * Created: 2/27/2023 6:33:19 PM
+ * Author : minas
+ */ 
+
+#define F_CPU 1000000UL
+#include "HAL/LCD_DRIVER/LCD.h"
+#include "MCAL/Include/PORT_DDR.h"
+#include "util/delay.h"
+
+void wait(int t)
+{
+	int i , j;
+	for(i = 0 ; i < t ; i++)
+	for(j = 0 ; j < 255 ; j++);
+}
+
+int main(void)
+{	
+	//Port_Mode(PORTA,OUTPUT);
+	LCD_clear_screen();
+	LCD_init();
+	LCD_Send_A_String("Learn in Depth ..");
+
+
+
+	/* Replace with your application code */
+	while (1)
+	{
+
+	}
+}
+
